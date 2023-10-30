fc64404de991ff17afdeb5130416e4fa5388830b
AbhayParasharhere <83302622+AbhayParasharhere@users.noreply.github.com>
2023-10-29 21:47:22 -0700

    Add download functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
-import React from "react"
-import SignUp from "./Sign-up-page/sign-up"
-import Login from "./Login-page/Login"
-import PosterPage, {loader as posterPageLoader}from "./PosterSelectorPage/PosterPage"
-import Poster1 , {loader as poster1Loader} from "../Posters/Poster-1/Poster1"
-import Poster2, {loader as poster2Loader}from "../Posters/Poster-2/Poster2"
-import {Route,createBrowserRouter,createRoutesFromElements,RouterProvider} from "react-router-dom"
-export default function App(){
-  const router = createBrowserRouter(createRoutesFromElements(
-            <Route>
-              <Route path = "/"element = {<PosterPage />} loader = {posterPageLoader}>
-                <Route index element = {<Poster1 />} loader = {poster1Loader} />
-                <Route  path = "poster-2" element = {<Poster2 />} loader = {poster2Loader}/>
-              </Route>
-              <Route element = {<Login />} path = "/login"/>
-              <Route element = {<SignUp />} path = "/signup"/>
-            </Route>
-  ))
-  return(
-    <RouterProvider router = {router}/>
-  )
+import React from "react";
+import SignUp from "./Sign-up-page/sign-up";
+import Login from "./Login-page/Login";
+import PosterPage, {
+  loader as posterPageLoader,
+} from "./PosterSelectorPage/PosterPage";
+import Poster1, { loader as poster1Loader } from "../Posters/Poster-1/Poster1";
+import Poster2, { loader as poster2Loader } from "../Posters/Poster-2/Poster2";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+export default function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<PosterPage />} loader={posterPageLoader}>
+          <Route index element={<Poster1 />} loader={poster1Loader} />
+          <Route path="poster-2" element={<Poster2 />} loader={poster2Loader} />
+        </Route>
+        <Route element={<Login />} path="/login" />
+        <Route element={<SignUp />} path="/signup" />
+      </Route>
+    )
+  );
+  return <RouterProvider router={router} />;
 }
-
--- src/PosterSelectorPage/PosterPage.jsx
+++ src/PosterSelectorPage/PosterPage.jsx
@@ -1,124 +1,197 @@
-import React from "react"
-import Header from "./PosterPageComponents/Header/Header"
-import SideBar from "./PosterPageComponents/SideBar/SideBar"
-import "./PosterPageComponents/PosterDisplayComponent/PosterDisplay.css"
-import { Outlet , redirect} from "react-router-dom"
-import Cookies from "js-cookie"
-export function loader(){
-    const token = Cookies.get("token")
-    if(!token){
-        throw redirect("/login")
-    } else{
-        return null
-    }
+import React, { useState } from "react";
+import Header from "./PosterPageComponents/Header/Header";
+import SideBar from "./PosterPageComponents/SideBar/SideBar";
+import "./PosterPageComponents/PosterDisplayComponent/PosterDisplay.css";
+import { Outlet, redirect } from "react-router-dom";
+import Cookies from "js-cookie";
+import html2canvas from "html2canvas";
+import domtoimage from "dom-to-image-more";
+
+export function loader() {
+  const token = Cookies.get("token");
+  if (!token) {
+    throw redirect("/login");
+  } else {
+    return null;
+  }
 }
-export default function PosterPage(){
-    const [posterSize,setPosterSize] = React.useState({
-        "Instagram": true,
-        "LinkedIn": false,
-        "Facebook":false,
-        "Twitter":false
-    })  
-    let selectSize = "Instagram"
-     for (const [key,value] of Object.entries(posterSize)) {
-        if(value){
-            selectSize = key
+export default function PosterPage() {
+  console.log("RENDERED");
+  const [posterSize, setPosterSize] = React.useState({
+    Instagram: true,
+    LinkedIn: false,
+    Facebook: false,
+    Twitter: false,
+  });
+  let selectSize = "Instagram";
+  const [download, setDownload] = useState(false);
+  for (const [key, value] of Object.entries(posterSize)) {
+    if (value) {
+      selectSize = key;
+    }
+  }
+
+  const sizeStyles = {
+    Twitter: {
+      width: "550px",
+      height: "290px",
+      containerType: "inline-size",
+      containerName: "changeRes",
+    },
+
+    Instagram: {
+      width: "300px",
+      height: "300px",
+      containerType: "inline-size",
+      containerName: "changeRes",
+    },
+    Facebook: {
+      width: "300px",
+      height: "300px",
+      containerType: "inline-size",
+      containerName: "changeRes",
+    },
+    LinkedIn: {
+      width: "350px",
+      height: "350px",
+      containerType: "inline-size",
+      containerName: "changeRes",
+    },
+  };
+  const sizePixles = {
+    Instagram: "1080px x 1080px",
+    Facebook: "1080px x 1080px",
+    LinkedIn: "1200px x 1200px",
+    Twitter: "1600px x 900px",
+  };
+  function handleSizeChange(platform) {
+    setPosterSize((prevState) => {
+      const obj = { ...prevState };
+      for (const key in obj) {
+        if (key !== platform) {
+          obj[key] = false;
         }
-    }
-    
-    const sizeStyles = {"Twitter": {"width":"550px",
-                        "height":"290px",
-                        "containerType": "inline-size",
-                        "containerName": "changeRes"}, 
+      }
+      obj[platform] = true;
+      return obj;
+    });
+  }
 
-                        "Instagram": {
-                            "width":"300px",
-                            "height":"300px",
-                            "containerType": "inline-size",
-                            "containerName": "changeRes"},
-                        "Facebook": {
-                            "width":"300px",
-                            "height":"300px",
-                            "containerType": "inline-size",
-                            "containerName": "changeRes"},
-                        "LinkedIn": {
-                            "width":"350px",
-                            "height":"350px",
-                            "containerType": "inline-size",
-                            "containerName": "changeRes"}
+  const downloadImage = () => {
+    const target = document.getElementById("poster-download");
+    const originalWidth = target.style.width;
+    const originalHeight = target.style.height;
 
-    }
-    const sizePixles = {"Instagram":"1080px x 1080px",
-                        "Facebook":"1080px x 1080px",
-                        "LinkedIn":"1200px x 1200px",
-                        "Twitter" : "1600px x 900px"}
-    function handleSizeChange(platform)
-    {
-        setPosterSize((prevState) => { 
-            const obj = {...prevState}
-                for (const key in obj) {
-                    if (key !== platform) {
-                      obj[key] = false;
-                    }
-                  }
-                obj[platform] = true; 
-                return obj
-    })}
-    return(
-        <div>
-            <Header />
-            <div className = "main-body-container">
-                <SideBar />
-                <div className = "main-poster-container">
-                    <div className = "poster--download-button-container">
-                        <div className = "poster--size">
-                            <span className = "black-color">Size-{selectSize}:</span> {sizePixles[selectSize]}
-                        </div>
-                        <div className = "main-button-container">
-                            <button className = "download-button">Download</button>
-                        </div>
-                    </div>
-                    <div className = "poster--display">
-                        <div className = "poster" style = {sizeStyles[selectSize]}><Outlet 
-                            text = "123456"
-                        /></div>
-                    </div>
+    setDownload(true);
 
-                    <div className = "poster--size-select-container">
-                        <div className="size-select-sub-container">
+    target.style.width = "1600px";
+    target.style.height = "900px";
+    domtoimage
+      .toPng(target)
+      .then((dataUrl) => {
+        var anchor = document.createElement("a");
+        anchor.setAttribute("href", dataUrl);
+        anchor.setAttribute("download", "my-image.png");
+        anchor.click();
+      })
+      .catch((error) => {
+        console.error("Error capturing the image: ", error);
+      })
+      .finally(() => {
+        target.style.width = originalWidth;
+        target.style.height = originalHeight;
+        setDownload(false);
+      });
+  };
 
-                            <div 
-                            className = {posterSize["Instagram"] ?"specific-size-select-container-selected" :"specific-size-select-container"} 
-                            onClick = {()=> {handleSizeChange("Instagram")}}>
+  return (
+    <div>
+      <Header />
+      <div className="main-body-container">
+        <SideBar />
+        <div className="main-poster-container">
+          <div className="poster--download-button-container">
+            <div className="poster--size">
+              <span className="black-color">Size-{selectSize}:</span>{" "}
+              {sizePixles[selectSize]}
+            </div>
+            <div className="main-button-container">
+              <button className="download-button" onClick={downloadImage}>
+                Download
+              </button>
+            </div>
+          </div>
+          <div className="poster--display">
+            <div
+              className="poster"
+              style={sizeStyles[selectSize]}
+              id="poster-download"
+            >
+              <Outlet text="123456" />
+            </div>
+          </div>
 
-                                <div className = "instagram-select" >.</div>
-                                <span>Instagram</span>
-                            </div>
+          <div className="poster--size-select-container">
+            <div className="size-select-sub-container">
+              <div
+                className={
+                  posterSize["Instagram"]
+                    ? "specific-size-select-container-selected"
+                    : "specific-size-select-container"
+                }
+                onClick={() => {
+                  handleSizeChange("Instagram");
+                }}
+              >
+                <div className="instagram-select">.</div>
+                <span>Instagram</span>
+              </div>
 
-                            <div className =  {posterSize["Facebook"]? "specific-size-select-container-selected" :"specific-size-select-container"} 
-                            onClick = {()=> {handleSizeChange("Facebook")}}
-                            >
-                                <div className = "facebook-select">.</div>
-                                <span>Facebook</span>
-                            </div>
+              <div
+                className={
+                  posterSize["Facebook"]
+                    ? "specific-size-select-container-selected"
+                    : "specific-size-select-container"
+                }
+                onClick={() => {
+                  handleSizeChange("Facebook");
+                }}
+              >
+                <div className="facebook-select">.</div>
+                <span>Facebook</span>
+              </div>
 
-                            <div className =  {posterSize["LinkedIn"] ? "specific-size-select-container-selected" :"specific-size-select-container"} 
-                            
-                            onClick = {()=> {handleSizeChange("LinkedIn")}}>
-                                <div className = "linkedIn-select">.</div>
-                                <span>LinkedIn</span>
-                            </div>
+              <div
+                className={
+                  posterSize["LinkedIn"]
+                    ? "specific-size-select-container-selected"
+                    : "specific-size-select-container"
+                }
+                onClick={() => {
+                  handleSizeChange("LinkedIn");
+                }}
+              >
+                <div className="linkedIn-select">.</div>
+                <span>LinkedIn</span>
+              </div>
 
-                            <div className =  {posterSize["Twitter"] ? "specific-size-select-container-selected" :"specific-size-select-container"} 
-
-                             onClick = {()=> {handleSizeChange("Twitter")}}>
-                                <div className = "twitter-select">.</div>
-                                <span>Twitter</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>  
+              <div
+                className={
+                  posterSize["Twitter"]
+                    ? "specific-size-select-container-selected"
+                    : "specific-size-select-container"
+                }
+                onClick={() => {
+                  handleSizeChange("Twitter");
+                }}
+              >
+                <div className="twitter-select">.</div>
+                <span>Twitter</span>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
