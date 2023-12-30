ca13cdc754cd818b1aef91bb8f00b561f46566ff
abhi-rzoro <144512510+abhi-rzoro@users.noreply.github.com>
2023-12-30 08:17:31 +0530

    Sidebar complete nw design

--- src/PosterSelectorPage/PosterPageComponents/SideBar/SideBar.jsx
+++ src/PosterSelectorPage/PosterPageComponents/SideBar/SideBar.jsx
@@ -33,17 +33,19 @@ export default function SideBar() {
     );
   });
   return (
-    <div className="main-sidebar-container">
-      <div className="sidebar--template-container">
-        <img className="sidebar--template-icon" src={templateIcon} />
-        <p className="sidebar--template-text"> Templates</p>
-      </div>
-      <hr className="sidebar--line-break" />
-      <select name="filter--dropdown" className="sidebar--dropdown">
-        <option value="new-year">New Year</option>
-      </select>
-      <div className="sidebar--display-preview-container">
-        {displayPreviewPoster}
+    <div className="sidebar--scroll-container">
+      <div className="main-sidebar-container">
+        <div className="sidebar--template-container">
+          <img className="sidebar--template-icon" src={templateIcon} />
+          <p className="sidebar--template-text"> Templates</p>
+        </div>
+        <hr className="sidebar--line-break" />
+        <select name="filter--dropdown" className="sidebar--dropdown">
+          <option value="new-year">New Year</option>
+        </select>
+        <div className="sidebar--display-preview-container">
+          {displayPreviewPoster}
+        </div>
       </div>
     </div>
   );
