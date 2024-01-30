009fcf82811a7c113a5b33f0e03eabda9ada3014
Naman <116908089+nbatra752@users.noreply.github.com>
2024-01-30 20:26:22 +0530

    Html2canvas options1

--- src/PosterSelectorPage/PosterPage.jsx
+++ src/PosterSelectorPage/PosterPage.jsx
@@ -108,9 +108,8 @@ export default function PosterPage() {
 
     document.body.appendChild(clone); // Add the clone to the document temporarily
 
-    html2canvas(clone, {
-      allowTaint : true
-    }).then((canvas) => {
+    html2canvas(clone)
+    .then((canvas) => {
             // Convert canvas to data URL
             const dataUrl = canvas.toDataURL();
 
