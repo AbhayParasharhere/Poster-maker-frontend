dbce085a7b19920db023125093e7e7503e50f772
Naman <116908089+nbatra752@users.noreply.github.com>
2024-02-07 09:56:59 +0530

    html2canvassfarai5

--- src/PosterSelectorPage/PosterPage.jsx
+++ src/PosterSelectorPage/PosterPage.jsx
@@ -107,7 +107,7 @@ export default function PosterPage() {
   
       document.body.appendChild(clone); // Add the clone to the document temporarily
 
-    html2canvas(clone, { useCORS: false })
+    html2canvas(clone, { useCORS: false, foreignObjectRendering: true })
     .then((canvas) => {
     // Convert the canvas to a data URL
     var dataUrl = canvas.toDataURL("image/png");
