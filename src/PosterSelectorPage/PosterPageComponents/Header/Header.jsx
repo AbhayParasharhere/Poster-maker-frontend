f71e356bdcac2ef72022404ea45c3cf60af70b48
abhi-rzoro <144512510+abhi-rzoro@users.noreply.github.com>
2024-03-02 09:44:44 +0530

    add sidebar header menu

--- src/PosterSelectorPage/PosterPageComponents/Header/Header.jsx
+++ src/PosterSelectorPage/PosterPageComponents/Header/Header.jsx
@@ -1,29 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import Cookies from "js-cookie";
 import { Navigate } from "react-router-dom";
 import logo from "/src/assets/images/posterMakerLogo.png";
+import profileImage from "./Ellipse.png";
 
 export default function Header() {
-  const [loggedIn, setLoggedIn] = React.useState(
-    Cookies.get("token") ? true : false
-  );
-  if (!loggedIn) {
-    return <Navigate to="/login" />;
-  }
+  const [loggedIn, setLoggedIn] = useState(Cookies.get("token") ? true : false);
+  const [dropdown, setDropdown] = useState(false);
+
   function logout() {
     Cookies.remove("token");
     setLoggedIn(false);
   }
+
+  if (!loggedIn) {
+    return <Navigate to="/login" />;
+  }
+
   return (
-    <div className="header">
-      <div>
-        <img className="header--logo-image" src={logo} />
+    <>
+      <div className="header">
+        <div>
+          <img className="header--logo-image" src={logo} alt="Logo" />
+        </div>
+        <div className="header--icon-container">
+          <button
+            onClick={() => setDropdown((prevDropdown) => !prevDropdown)}
+            className="log-out-button"
+          >
+            Options{" "}
+          </button>
+        </div>
       </div>
-      <div className="header--icon-container">
-        <button onClick={logout} className="log-out-button">
-          Log Out{" "}
-        </button>
+      <div className={dropdown ? "header--dropdown" : "display-none"}>
+        <div className="header--dropdown-profile-container">
+          <img
+            src={profileImage}
+            className="header--dropdown-profile-image"
+            alt="Profile"
+          />
+          <p className="header--dropdown-profile-text">Your Profile</p>
+        </div>
+        <hr className="header--dropdown-linebreak" />
+        <div className="header--dropdown-menu-container">
+          <p className="header--dropdown-menu-text">Change Details</p>
+          <hr className="header--dropdown-menu-linebreak" />
+          <p className="header--dropdown-menu-text">Need Help?</p>
+          <hr className="header--dropdown-menu-linebreak" />
+          <p className="header--dropdown-menu-text" onClick={logout}>
+            Log Out
+          </p>
+          <hr className="header--dropdown-menu-linebreak" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
