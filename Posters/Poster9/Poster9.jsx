231b597e272e21e31b8e1b87f96dab291e4ccdf0
AbhayParasharhere <83302622+AbhayParasharhere@users.noreply.github.com>
2024-03-05 20:03:27 -0800

    Add automatic aspect ratio

--- Posters/Poster9/Poster9.jsx
+++ Posters/Poster9/Poster9.jsx
@@ -14,29 +14,27 @@ export async function loader() {
 export default function Poster9() {
   const loaderData = useLoaderData();
   return (
-    /* <div class="containerPoster9"> */
-
-    <div class="maincontainerPoster9">
-      <div class="text1Poster9">
-        <p class="text1P9">2</p>
-        <img src={imgP9} class="imgP9" />
-        <p class="text2P9">2</p>
-        <p class="text1P9">4</p>
+    <div className="maincontainerPoster9">
+      <div className="text1Poster9">
+        <p className="text1P9">2</p>
+        <img src={imgP9} className="imgP9" />
+        <p className="text2P9">2</p>
+        <p className="text1P9">4</p>
       </div>
-      <div class="text2Poster9">HAPPY NEW YEAR</div>
-      <div class="text3Poster9">
-        <p class="text3P9">{loaderData.textData.name}</p>
-        <div class="text4P9">
-          <p class="text5P9">{loaderData.textData.contact_number}</p>
+      <div className="text2Poster9">HAPPY NEW YEAR</div>
+      <div className="text3Poster9">
+        <p className="text3P9">{loaderData.textData.name}</p>
+        <div className="text4P9">
+          <p className="text5P9">{loaderData.textData.contact_number}</p>
         </div>
       </div>
-      <div class="text4Poster9">
-        <p class="text6P99">{loaderData.textData.designation}</p>
-        <p class="text7P9">WWW.PUNJABINSURANCE.CA</p>
+      <div className="text4Poster9">
+        <p className="text6P99">{loaderData.textData.designation}</p>
+        <p className="text7P9">WWW.PUNJABINSURANCE.CA</p>
       </div>
       <img
         src={loaderData?.personImage?.background_image || imgP9}
-        class="empP9"
+        className="aspectFix empP9"
       />
     </div>
   );
